056e2b9eebb58886f7008250b7fd40e3bda4ae8f
Chandankn <87237018+Chandankn@users.noreply.github.com>
2021-07-14 08:26:50 -0700

    Created by chandan
    developed by the Mississippi.py i string functions

---
+++ Mississippi.py
@@ -0,0 +1,16 @@
+#include<stdio.h>
+S = input("Enter the Word Mississippi")
+C = input("Enter the Character miSSiSSippi")
+
+str = "Mississippi"
+for i in range (len(S)):
+    if S[i] == C:
+       str += S[i].upper)
+    else:
+       str +=S[i]
+ print(str)      
+ 
+ Output : S = 04
+          i = 04
+          p = 02
+          m = 01
